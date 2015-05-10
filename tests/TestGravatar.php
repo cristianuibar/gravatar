39edcb3dda722dacbf7fa9b72c53b7adb4225ebc
Uibar Ion-Cristian <uibar@vision-gate.com>
2015-05-10 20:35:29 +0300

    Basic Tests

---
+++ tests/TestGravatar.php
@@ -0,0 +1,22 @@
+<?php
+
+use Uibar\Gravatar\Gravatar;
+
+use \Uibar\Gravatar\Facades\Gravatar as GravatarStatic;
+
+class TestGravatar extends \PHPUnit_Framework_TestCase
+{
+    public function testGravatarGetFunction()
+    {
+        $gravatar = new Gravatar();
+
+        $url = $gravatar->get('test@email.com');
+
+        $this->assertNotEmpty($url);
+    }
+
+    public function testMethodChaining()
+    {
+        GravatarStatic::shouldReceive('email')->once()->andReturnSelf();
+    }
+}
